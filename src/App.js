76e7e047e62a9eeeb91e0b80278b0267f7489cad
e3c8f1a924 <zhhahhsh@outlook.com>
2022-10-01 11:21:36 +0800

    add: 获取数据

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState({ ready: 0 });
+
+  async function loadData() {
+    let contestsRes = await fetch("/data.json");
+    let contests = await contestsRes.json();
+    return {
+      contests: contests,
+      ready: 1
+    }
+  }
+
+  if (!data.ready) {
+    loadData()
+      .then(data => {
+        setData(data)
+      })
+      .catch(e => console.log(e));
+  }
+
   return (
     <div className="App">
       <HashRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home data={data}/>} />
         </Routes>
       </HashRouter>
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './Home.css';
 
-function Home() {
+function Home(props) {
   return (
     <div className="Home">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {props.data.ready ? JSON.stringify(props.data) : "Loading..."}
     </div>
   );
 }
